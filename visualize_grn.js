18074c9c0ab9807c4156e8b6edd60adb30ecfe92
Dillon Scott <dillonscott.2024@gmail.com>
2025-04-15 02:23:33 -0400

    add searching feature and textual display

--- visualize_grn.js
+++ visualize_grn.js
@@ -14,16 +14,25 @@ const typeToSymbol = {
 }
 
 const build = async (grnPath) => {
-  const data = await (fetch(grnPath).then(grnData => grnData.json()));
+  const data = await d3.json(grnPath);
 
   // Specify the color scale.
   const color = d3.scaleOrdinal(d3.schemeCategory10);
 
   // The force simulation mutates links and nodes, so create a copy
   // so that re-evaluating this cell produces the same result.
-  const nodes = Object.entries(data.nodes).map(([id, node]) => ({id: id, ...node})).filter(node => node.condition_sensitivity > 0.05);
-  const nodeIdSet = new Set(nodes.map(n => n.id))
-  const links = Object.entries(data.edges).flatMap(([source, targets]) => targets.map(t => ({source: source, target: String(t)}))).filter(edge => nodeIdSet.has(edge.source) && nodeIdSet.has(edge.target));
+  const allNodes = data.nodes.map(node => ({...node, id: String(node.id)})).filter(node => node.condition_sensitivity > 0.05);
+  const allLinks = Object.entries(data.edges).flatMap(([source, targets]) => targets.map(t => ({source: String(source), target: String(t)})));
+  let nodes = allNodes;
+  let links = allLinks;
+  let nodeLength = nodes.length + 1
+  while (nodeLength !== nodes.length) {
+    nodeLength = nodes.length
+    const nodeIdSet = new Set(nodes.map(n => n.id));
+    links = links.filter(edge => nodeIdSet.has(edge.source) && nodeIdSet.has(edge.target));
+    links.forEach(edge => nodeIdSet.delete(edge.source) && nodeIdSet.delete(edge.target));
+    nodes = nodes.filter(node => !nodeIdSet.has(node.id));
+  }
 
   // Create a simulation with several forces.
   const simulation = d3.forceSimulation(nodes)
@@ -32,18 +41,33 @@ const build = async (grnPath) => {
       .force("x", d3.forceX())
       .force("y", d3.forceY());
 
+  const container = d3.create("div")
+
   // Create the SVG container.
-  const outerSvg = d3.create("svg")
+  const outerSvg = container.append("svg")
       .attr("width", width)
       .attr("height", height)
       .attr("viewBox", [-width, -height, width * 2, height * 2])
       .attr("style", "max-width: 100%; height: auto;");
 
+
+  const searchText = container.append("input").attr("type", "text");
+  const searchButton = container.append("button").text("Go");
+
+  const displayText = container.append("xhtml:body");
+
+
   // Add a zoom behavior to container
-  const zoom = d3.zoom().scaleExtent([1, 5]).translateExtent([[-width, -height], [width, height]])
+  const zoom = d3.zoom().scaleExtent([1, 5]).translateExtent([[-width, -height], [width * 2, height * 2]])
   // outerSvg.call(zoom
   //   .on("zoom", (evt) => {
-  //     outerSvg.attr("transform", `translate(${evt.transform.x},${evt.transform.y}) scale(${evt.transform.k})`);
+  //     const prevViewBox = outerSvg.attr("viewBox").split(",").map(s => Number.parseInt(s))
+  //     const scale = evt.transform.k
+  //     console.log(prevViewBox)
+  //     console.log(evt.transform)
+  //     outerSvg.attr("viewBox", [[prevViewBox[0] - 2 * evt.dx, prevViewBox[1] - 2 * evt.dy], prevViewBox.slice(2, 4)])
+  //     // console.log(evt.transform)
+  //     // outerSvg.attr("transform", `translate(${evt.transform.x},${evt.transform.y}) scale(${evt.transform.k})`);
   //   })
   // );
 
@@ -57,12 +81,12 @@ const build = async (grnPath) => {
   //   })
   // );
 
-  outerSvg.call(d3.drag()
-    .on("drag", (evt) => {
-      const prevViewBox = outerSvg.attr("viewBox").split(",").map(s => Number.parseInt(s))
-      outerSvg.attr("viewBox", [[prevViewBox[0] - 2 * evt.dx, prevViewBox[1] - 2 * evt.dy], prevViewBox.slice(2, 4)])
-    })
-  );
+  // outerSvg.call(d3.drag()
+  //   .on("drag", (evt) => {
+  //     const prevViewBox = outerSvg.attr("viewBox").split(",").map(s => Number.parseInt(s))
+  //     outerSvg.attr("viewBox", [[prevViewBox[0] - 2 * evt.dx, prevViewBox[1] - 2 * evt.dy], prevViewBox.slice(2, 4)])
+  //   })
+  // );
 
   const svg = outerSvg.append("g");
 
@@ -93,17 +117,19 @@ const build = async (grnPath) => {
     .selectAll("path")
     .data(nodes)
     .join("path")
+      .attr("id", d => `id${d.id}`)
       .attr("fill", d => color(d.organ_specificity > d.universality))
       .attr("d", d => typeToSymbol[d.node_type].size(100)());
-
-  node.append("title")
-      .text(d => d.id);
 
   // Add a drag behavior.
   node.call(d3.drag()
         .on("start", dragstarted)
         .on("drag", dragged)
         .on("end", dragended));
+
+  node.on("click", (_evt, d) => selectNode(d.id))
+
+  searchButton.on("click", (_evt) => selectNode(searchText.property("value")))
   
   // Set the position attributes of links and nodes each time the simulation ticks.
   simulation.on("tick", () => {
@@ -138,12 +164,31 @@ const build = async (grnPath) => {
     event.subject.fy = null;
   }
 
+  // Zoom to a node with the given data, show data in displayText
+  function selectNode(nodeId) {
+    const d = nodes.find(n => n.id === nodeId)
+    if (!d) {
+      displayText.html(`Gene/Peak ${nodeId} not found.`)
+      return
+    }
+    node.attr("stroke", "#fff")
+    svg.select(`#id${d.id}`).attr("stroke", "red")
+    displayText.html(d.node_type === 'peak' ? 
+        `Peak: ${d.id}<br/>Condition sensitivity score: ${d.condition_sensitivity.toFixed(5)}<br/>Organ specificity score: ${d.organ_specificity.toFixed(5)}<br/>Universality score: ${d.universality.toFixed(5)}` : 
+        `Gene: ${d.id}<br/>Condition sensitivity score: ${d.condition_sensitivity.toFixed(5)}<br/>Organ specificity score: ${d.organ_specificity.toFixed(5)}<br/>Universality score: ${d.universality.toFixed(5)}`);
+
+    svg.transition().duration(700).call(
+      zoom.transform,
+      d3.zoomIdentity.translate((width / 2), (height / 2)).scale(4).translate(-d.x, -d.y),
+    )
+  }
+
   // When this cell is re-run, stop the previous simulation. (This doesn’t
   // really matter since the target alpha is zero and the simulation will
   // stop naturally, but it’s a good practice.)
   // invalidation.then(() => simulation.stop());
 
-  return outerSvg.node();
+  return container.node();
 }
 
 export { build };
