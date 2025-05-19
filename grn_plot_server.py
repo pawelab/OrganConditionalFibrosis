ed1384473ef6a2010259cfefe2eb8888cc195880
Dillon Scott <dillonscott.2024@gmail.com>
2025-05-19 00:44:22 -0400

    test gunicorn deploy

--- grn_plot_server.py
+++ grn_plot_server.py
@@ -2,7 +2,7 @@
 Sets up a server that takes in nodes from the grn in `visualize_grn.js`, plots their behavior, and places figures in a temp directory.
 """
 
-from flask import Flask, request, send_from_directory
+from flask import Flask, request
 from tempfile import TemporaryDirectory
 import os
 from pathlib import Path
@@ -10,7 +10,7 @@ from plot_utils import plot_expression, plot_peak
 import argparse
 import anndata as ad
 
-if __name__ == '__main__':
+if __name__ == '__main__':  # run locally
   parser = argparse.ArgumentParser()
   parser.add_argument("--rna_adata", help="Path to the bulk RNA AnnData file", type=Path, default='./data/rna.h5ad')
   parser.add_argument("--atac_adata", help="Path to the bulk ATAC AnnData file", type=Path, default='./data/atac.h5ad')
@@ -37,3 +37,23 @@ if __name__ == '__main__':
       return { 'path': temp_file_path }
 
     app.run()
+else:  # run on server
+  print(os.environ['rna_adata'])
+  rna_ad = ad.AnnData()
+  atac_ad = ad.AnnData()
+  # rna_ad = ad.read_h5ad(os.environ['rna_adata'])
+  # atac_ad = ad.read_h5ad(os.environ['atac_adata'])
+
+  app = Flask(__name__, static_url_path='', static_folder='./')
+
+  @app.route('/plot', methods = ['POST'])
+  def plot():
+    data = request.get_json()
+    node_id = data['id']
+    is_gene = data['node_type'] == 'gene'
+    temp_file_path = os.path.join('/tmp', f"{node_id}.png")
+    if not os.path.exists(temp_file_path):
+      ax = plot_expression(rna_ad, node_id) if is_gene else plot_peak(atac_ad, f'Interval_{node_id}')
+      ax.figure.savefig(temp_file_path)
+      ax.figure.clear()
+    return { 'path': temp_file_path }
